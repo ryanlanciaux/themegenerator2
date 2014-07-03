8f9c4cf900020a736142b45207c99d01aa5d2fef
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2014-07-03 16:31:08 -0400

    Well Visual Studio uses BGR instead of RGB. That certainly explains some things.

--- scripts/theme-list.js
+++ scripts/theme-list.js
@@ -8,19 +8,21 @@ themeList.factory('themeList', function(){
 		},
 		{name: "Visual Studio 2013", path: "/themes/visual-studio-2013.html", extension: '.vssettings', format: function(themeData){
 			var colors = JSON.parse(JSON.stringify(themeData()));
+			//this is dumb but vssettings are BGR instead of RGB.
 				return {
-					main: colors.main.replace("#", ""),
-					foreground: colors.foreground.replace("#", ""),
-					background: colors.background.replace("#", ""),
-					color1: colors.color1.replace("#", ""),
-					color2: colors.color2.replace("#", ""),
-					color3: colors.color3.replace("#", ""),
-					color4: colors.color4.replace("#", ""),
-					color5: colors.color5.replace("#", ""),
-					color6: colors.color6.replace("#", ""),
-					color7: colors.color7.replace("#", ""),
-					color8: colors.color8.replace("#", ""),
-					color9: colors.color9.replace("#", ""),
+					main: colors.main.substring(5,8) + colors.main.substring(3, 5) + colors.main.substring(1,3),
+					foreground: colors.foreground.substring(5,8) + colors.foreground.substring(3, 5) + colors.foreground.substring(1,3),
+					background: colors.background.substring(5,8) + colors.background.substring(3, 5) + colors.background.substring(1,3),
+					comment: colors.comment.substring(5,8) + colors.comment.substring(3, 5) + colors.comment.substring(1,3),
+					color1: colors.color1.substring(5,8) + colors.color1.substring(3, 5) + colors.color1.substring(1,3),
+					color2: colors.color2.substring(5,8) + colors.color2.substring(3, 5) + colors.color2.substring(1,3),
+					color3: colors.color3.substring(5,8) + colors.color3.substring(3, 5) + colors.color3.substring(1,3),
+					color4: colors.color4.substring(5,8) + colors.color4.substring(3, 5) + colors.color4.substring(1,3),
+					color5: colors.color5.substring(5,8) + colors.color5.substring(3, 5) + colors.color5.substring(1,3),
+					color6: colors.color6.substring(5,8) + colors.color6.substring(3, 5) + colors.color6.substring(1,3),
+					color7: colors.color7.substring(5,8) + colors.color7.substring(3, 5) + colors.color7.substring(1,3),
+					color8: colors.color8.substring(5,8) + colors.color8.substring(3, 5) + colors.color8.substring(1,3),
+					color9: colors.color9.substring(5,8) + colors.color9.substring(3, 5) + colors.color9.substring(1,3)
 				};
 			}
 		}
